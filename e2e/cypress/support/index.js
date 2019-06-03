89c96f910d4553d17ad7484ef21ebc32acc1bc25
Johannes Steu <hello@johannessteu.de>
2019-06-03 10:48:04 +0200

    Fix cypress import

--- e2e/cypress/support/index.js
+++ e2e/cypress/support/index.js
@@ -1,5 +1,5 @@
 // <reference types="Cypress" />
 // <reference types="Cypress-testing-library" />
 
-import 'cypress-testing-library/add-commands';
+import '@testing-library/cypress/add-commands';
 import './commands';
