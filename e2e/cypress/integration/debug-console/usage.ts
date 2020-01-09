8adba97404f33ee1fa5209787bd1f6dd64f2263e
Johannes Steu <hello@johannessteu.de>
2020-01-09 08:52:11 +0100

    Check for „inspect“ instead of „render time“ in e2e tests

--- e2e/cypress/integration/debug-console/usage.ts
+++ e2e/cypress/integration/debug-console/usage.ts
@@ -1,56 +1,56 @@
 /// <reference types="Cypress" />
 /// <reference types="Cypress-testing-library" />
 
-describe('debug console can be used at all', () => {
-  it('console can be opened and closed', () => {
+describe("debug console can be used at all", () => {
+  it("console can be opened and closed", () => {
     // the console is not there yet+
-    cy.visit('/')
+    cy.visit("/")
       .queryByText(/render time/i)
-      .should('not.exist');
+      .should("not.exist");
 
     // bring up the console
     cy.openConsole()
       .queryByText(/render time/i)
-      .should('exist')
+      .should("exist")
       .queryByText(/close/i)
-      .should('exist');
+      .should("exist");
 
     // close it
     cy.window()
       .queryByText(/close/i)
       .click()
       .queryByText(/render time/i)
-      .should('not.exist');
+      .should("not.exist");
   });
 
-  it.only('cookie can be set so console stays on a refresh', () => {
-    cy.visit('/')
-      .queryByText(/render time/i)
-      .should('not.exist');
+  it.only("cookie can be set so console stays on a refresh", () => {
+    cy.visit("/")
+      .queryByText(/inspect/i)
+      .should("not.exist");
 
     // bring up the console
     cy.openConsole(true)
-      .queryByText(/render time/i)
-      .should('exist')
-      .getCookie('__neos_debug__')
-      .should('have.property', 'value')
-      .should('eq', 'true')
-      .queryByText(/render time/i)
-      .should('exist');
+      .queryByText(/inspect/i)
+      .should("exist")
+      .getCookie("__neos_debug__")
+      .should("have.property", "value")
+      .should("eq", "true")
+      .queryByText(/inspect/i)
+      .should("exist");
   });
 
-  it('debug console opens when the cookie is set', () => {
-    cy.setCookie('__neos_debug__', 'true')
-      .visit('/')
-      .queryByText(/render time/i);
+  it("debug console opens when the cookie is set", () => {
+    cy.setCookie("__neos_debug__", "true")
+      .visit("/")
+      .queryByText(/inspect/i);
   });
 
-  it('closing the console will also delete the cooke', () => {
-    cy.setCookie('__neos_debug__', 'true')
-      .visit('/')
+  it("closing the console will also delete the cooke", () => {
+    cy.setCookie("__neos_debug__", "true")
+      .visit("/")
       .queryByText(/close/i)
       .click()
-      .getCookie('__neos_debug__')
-      .should('eq', null);
+      .getCookie("__neos_debug__")
+      .should("eq", null);
   });
 });
